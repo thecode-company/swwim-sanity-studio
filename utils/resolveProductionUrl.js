87d5fd526da9e919d5394f97367eb98cd4774136
Sam Goddard <hello@samgoddard.co.uk>
2021-11-22 12:13:34 +0000

    seo live

--- utils/resolveProductionUrl.js
+++ utils/resolveProductionUrl.js
@@ -9,7 +9,7 @@ const secret = process.env.SANITY_STUDIO_SANITY_PREVIEW_SECRET
 export const getGlobalSlug = (slug) => globalSlugs[slug]
 
 // TODO: Change remote url
-const remoteURL = 'https://swwim.vercel.app'
+const remoteURL = 'https://www.weswwim.com'
 const localURL = 'http://localhost:3000'
 export const previewURL = window.location.hostname === 'localhost' ? localURL : remoteURL
 
